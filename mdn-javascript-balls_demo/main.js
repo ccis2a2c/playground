362784c0ac33209c489608f02055bdcf704fb0fc
CC <cc@cc.cc>
2019-05-14 23:02:17 +0800

    Original code from MDN

---
+++ mdn-javascript-balls_demo/main.js
@@ -0,0 +1,20 @@
+// 设定画布
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+// 设定画布长宽
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// 生成随机数的函数
+function random(min,max) {
+  return Math.floor(Math.random()*(max-min)) + min;
+}
+
+// 生成随机颜色的函数
+function randomColor() {
+  return 'rgb(' +
+         random(0, 255) + ', ' +
+         random(0, 255) + ', ' +
+         random(0, 255) + ')';
+}
