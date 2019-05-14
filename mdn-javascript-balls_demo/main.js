086f7ca4d7e0e8956fc0ae054cffa1dc39b99087
shajianrui <shajianrui@126.com>
2019-05-15 00:06:21 +0800

    0.1
    弹跳球的最初版本，完全按照MDN教程来实现。
    1. 有些生成在画布边缘的球会沿着边缘摩擦并鬼畜。
    2. 可以尝试实现碰撞。

--- mdn-javascript-balls_demo/main.js
+++ mdn-javascript-balls_demo/main.js
@@ -18,3 +18,61 @@ function randomColor() {
          random(0, 255) + ', ' +
          random(0, 255) + ')';
 }
+
+function Ball(x, y, velX, velY, size, color) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.size = size;
+    this.color = color;
+}
+
+Ball.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+var balls = [];
+
+Ball.prototype.update = function() {
+    if (this.x + this.size >= width) {
+        this.velX = -(this.velX);
+    }
+    if (this.x - this.size <= 0) {
+        this.velX = -(this.velX);
+    }
+    if (this.y + this.size >= height) {
+        this.velY = -(this.velY);
+    }
+    if (this.y - this.size <= 0) {
+        this.velY = -(this.velY);
+    }
+    this.x += this.velX;
+    this.y += this.velY;
+}
+
+function loop() {
+    ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    ctx.fillRect(0, 0, width, height);
+    //
+    while (balls.length < 100) {
+        let ball = new Ball(
+            random(0, width),
+            random(0, height),
+            random(-20, 20),
+            random(-20, 20),
+            random(10, 20),
+            randomColor()
+        );
+        balls.push(ball);
+    }
+    //
+    for (let i = 0; i < balls.length; i += 1) {
+        balls[i].draw();
+        balls[i].update();  //两个函数执行的顺序不知道有没有什么坑。
+    }
+    requestAnimationFrame(loop);
+}
